a196acfdf093509327532b21a4b47ce4b086974c
Katharine Durbin <kdurbin1@jh.edu>
2022-09-06 12:09:55 -0400

    updated to correct version

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
 const colors = [
-  "white", 
-  "red", 
-  "white", 
-  "red", 
-  "white", 
-  "red"
+  "rgb(168, 39, 254)",
+  "rgb(154, 3, 254)",
+  "rgb(150, 105, 254)",
+  "rgb(140, 140, 255)",
+  "rgb(187, 187, 255)",
+  "rgb(143, 254, 221)",
 ]
+
+function createWakeUpTimeElement(time, cycle) {
+  let elm = document.createElement("div");
+  elm.innerText = time;
+  elm.style.color = colors[cycle];
+  return elm;
+}
 
 function handleOnClick() {
   let output = document.querySelector(".output");
@@ -14,16 +21,21 @@ function handleOnClick() {
   let hours = document.getElementById("hours");
   hours.innerHTML = "";
 
+  let hh = document.getElementById("hh").value;
+  let mm = document.getElementById("mm").value;
+  let ampm = document.getElementById("ampm").value;
+  hh = ampm === "PM" ? Number.parseInt(hh) + 12 : hh;
   let now = new Date();
-  // allow 14 minutes to fall sleep!
-  now.setMinutes(now.getMinutes() + 14);
+  now.setHours(hh);
+  now.setMinutes(mm);
 
   // calculate sleep cycles!
   for (let i = 1; i <= 6; i++) {
     now.setMinutes(now.getMinutes() + 90);
-    let elm = document.createElement("div");
-    elm.innerText = now.toLocaleTimeString("en-US", {timeStyle: "short" });
-    elm.color = colors[i - 1]; //FIXME
-    hours.innerHTML += now.toLocaleTimeString("en-US", { timeStyle: "short" }) + "<br />";
+    const elm = createWakeUpTimeElement(
+      now.toLocaleTimeString("en-US", { timeStyle: "short" }),
+      i
+    )
+    hours.appendChild(elm);
   } 
 }
